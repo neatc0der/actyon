9c0efae8ef4ec81960c5c59fbe8fd4126dd881ad
neatc0der <grosch.tobias@googlemail.com>
2021-02-11 21:05:52 +0100

    Fix for simplified interface

--- actyon/__init__.py
+++ actyon/__init__.py
@@ -1,6 +1,7 @@
 import inspect
+from asyncio.tasks import gather
 from logging import Logger
-from typing import Any, Callable, List, Type
+from typing import Any, Callable, Iterable, List, Type
 
 from .actyon import Actyon, ActyonError
 from .console import DisplayHook  # noqa: F401
@@ -11,21 +12,27 @@ from .hook import ActyonHook, HookEvent, HookEventType  # noqa: F401
 _log: Logger = _get_logger()
 
 
-async def execute(name: str, obj: Any) -> None:
-    actyon: Actyon = Actyon.get(name)
-    if actyon is None:
-        raise ActyonError(f"unknown actyon: {actyon}")
+async def execute(*names: Iterable[str], dependency: Any) -> None:
+    actyons: Iterable[Actyon] = []
+    for name in names:
+        actyon: Actyon = Actyon.get(name)
+        if actyon is None:
+            raise ActyonError(f"unknown actyon: {name}")
+        actyons.append(actyon)
 
-    try:
-        await actyon.execute(obj)
-    except ActyonError as e:
-        _log.exception(str(e))
+    if len(names) == 0:
+        raise ActyonError("no actyon name provided")
+
+    results: Iterable[Any] = await gather(*(actyon.execute(dependency) for actyon in actyons), return_exceptions=True)
+    for i, exc in enumerate(results):
+        if isinstance(exc, BaseException):
+            _log.exception(f"error while executing {actyons[i].name}", exc_info=exc)
 
 
 def produce(name: str) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
     def _inner(func: Callable[..., Any]) -> Callable[..., Any]:
         t: Type = inspect.signature(func).return_annotation
-        actyon: Actyon = Actyon.get_or_create('name', t)
+        actyon: Actyon = Actyon.get_or_create(name, t)
         return actyon.producer(func)
 
     if name is None or not isinstance(name, str):
@@ -38,7 +45,7 @@ def consume(name: str) -> Callable[[Callable[[List[Any]], None]], Callable[[List
     def _inner(func: Callable[..., Any]) -> Callable[..., Any]:
         t: Type = next(iter(inspect.signature(func).parameters.values())).annotation \
                   if len(inspect.signature(func).parameters) > 0 else None
-        actyon: Actyon = Actyon.get_or_create('name', t)
+        actyon: Actyon = Actyon.get_or_create(name, t)
         return actyon.consumer(func)
 
     if name is None or not isinstance(name, str):
