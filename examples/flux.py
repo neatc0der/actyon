2e1984e6b484bf22b205c857ebcebe8e823982be
neatc0der <grosch.tobias@googlemail.com>
2021-02-12 22:46:57 +0100

    Add example for flux

---
+++ examples/flux.py
@@ -0,0 +1,37 @@
+from asyncio import run
+
+from actyon.flux import Flux
+import attr
+
+
+@attr.s
+class Counter:
+    value = attr.ib()
+
+
+flux: Flux = Flux[Counter](
+    initial=Counter(0),
+)
+
+
+@flux.reducer
+async def increment(state: Counter, action: Flux.Action) -> Counter:
+    state.value += action.get("inc", 1)
+    print(state)
+    return state
+
+
+@flux.effect("increment")
+async def increment_effect(state: Counter) -> None:
+    if state.value < 10:
+        await flux.dispatch("increment", {"inc": state.value})
+
+
+async def main():
+    await flux.run()
+    await flux.dispatch("increment")
+    await flux.done()
+
+
+if __name__ == "__main__":
+    run(main())
